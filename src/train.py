d726730a019ba45e8876ca213c676e888bb05a28
Nikhil Reddy <nreddymit@gmail.com>
2025-04-12 13:15:52 +0000

    Policy Split

--- src/train.py
+++ src/train.py
@@ -313,7 +313,7 @@ def main():
         num_steps=60000 * 3 * 10,
         ckpt_frequency=1000 * 3,
         save_frequency=1000 * 3,
-        save_checkpoint_path='../checkpoints/policy-downweight/',
+        save_checkpoint_path='../checkpoints/policy-split/',
     )
     
     # Train model
